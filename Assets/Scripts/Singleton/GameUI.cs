50c440c5f4dd031f53ecdeed254039e9d645b5e1
Ray Tran <tranb2@spu.edu>
2019-04-18 19:40:42 -0700

    Fixed optimal meld bugs, removed Shuffle button

--- Assets/Scripts/Singleton/GameUI.cs
+++ Assets/Scripts/Singleton/GameUI.cs
@@ -16,16 +16,6 @@ public class GameUI : MonoBehaviour
         currentTurnText = scoreGO.GetComponent<Text>();
         currentTurnText.text = "Current turn: PlayerDraw";
         currentDeadwoodPointsText = deadpointsGO.GetComponent<Text>();
-
-
-	}
-
-	public void Shuffle()
-	{
-		if (Dealer.instance.DealInProgress == 0)
-		{
-            StartCoroutine(Dealer.instance.ShuffleCoroutine());
-        }
 	}
 
     //Called in Player.ScanHand() and Round.UpdateTurn()
