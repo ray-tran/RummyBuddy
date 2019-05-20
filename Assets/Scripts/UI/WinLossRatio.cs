9826ce48f4d62543caa71baa4269655f8ce4f962
MatthewAbbay <33332589+MatthewAbbay@users.noreply.github.com>
2019-05-19 19:50:22 -0700

    Create WinLossRatio.cs

---
+++ Assets/Scripts/UI/WinLossRatio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinLossRatio : MonoBehaviour
+{
+    int wins;
+    int losses;
+    int wlratio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wins = Match.instance.PlayerWinCount;
+        losses = Match.instance.PlayerLossCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        wins = Match.instance.PlayerWinCount;
+        losses = Match.instance.PlayerLossCount;
+
+        wlratio = wins / losses;
+
+        Text wl = this.GetComponent<Text>();
+
+        wl.text = "Win/Loss Ratio: " + wlratio;
+    }
+}
