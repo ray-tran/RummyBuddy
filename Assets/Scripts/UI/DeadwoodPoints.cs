d7332d62f3477b0a72c80af42e9ed0b91da4a8dc
MatthewAbbay <33332589+MatthewAbbay@users.noreply.github.com>
2019-05-27 21:05:01 -0700

    Create DeadwoodPoints.cs

---
+++ Assets/Scripts/UI/DeadwoodPoints.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeadwoodPoints : MonoBehaviour
+{
+    public int playerDeadwoodPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerDeadwoodPoints = PlayerHand.instance.DeadwoodPoints;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text score = this.GetComponent<Text>();
+        score.text = "Deadwood: " + playerDeadwoodPoints;
+    }
+}
