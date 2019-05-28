31ce1f02ac6e3771cfbbc52a7f7bf49223704485
MatthewAbbay <33332589+MatthewAbbay@users.noreply.github.com>
2019-05-27 21:04:52 -0700

    Create CurrentTrend.cs

---
+++ Assets/Scripts/UI/CurrentTrend.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentTrend : MonoBehaviour
+{
+    //public List<List<string>> currentTrend;
+    int currentTrend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //currentTrend = Match.instance.RoundResult;
+
+        Text display = this.GetComponent<Text>();
+
+        if (GameUI.instance.endMatch == true)
+        {
+           
+
+        }
+        
+    }
+}
