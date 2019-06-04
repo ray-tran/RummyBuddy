b831ddfbbb0b06de5069193ba38658c45ae47a29
Ray Tran <tranb2@spu.edu>
2019-06-03 21:49:57 -0700

    Resigned main menu. Added music. Added button secondary colors. Removed unnecessary files.

---
+++ Assets/Scripts/UI/GameButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class GameButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Sprite HighlightedImage;
+    public Sprite NormalImage;
+    public Button button;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        button.GetComponent<Image>().sprite = HighlightedImage;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        button.GetComponent<Image>().sprite = NormalImage;
+    }
+
+
+}
